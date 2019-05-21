11e9287b7a4fed1a5f98c9094b39fd152c94e498
ScWen7 <xxh783965@gmail.com>
2019-05-21 18:54:10 +0800

    main view

--- WanAndroid2/app/src/main/java/com/scwen/wanandroid/MainActivity.kt
+++ WanAndroid2/app/src/main/java/com/scwen/wanandroid/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.scwen.wanandroid
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import com.scwen.wanandroid.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun getLayoutRes(): Int = R.layout.activity_main
+
+    override fun initActivity() {
+        initImmBar()
+
     }
+
+    override fun initData() {
+    }
+
+
 }
